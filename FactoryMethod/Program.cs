c9c55009303ec819ad62db7d0fb629de673c874d
Henry-Roberto <henroberto@hotmail.com>
2025-02-08 23:41:50 -0300

    Adicionar arquivos de projeto.

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,52 @@
+﻿using FactoryMethod.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Transport transport = null;
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Selecione o tipo da entrega.");
+                Console.ReadLine();
+                return;
+            }
+
+            string arg = args[0];
+
+            switch (arg)
+            {
+                case "--uber":
+                    transport = new CarTransport();
+                    break;
+                case "--log":
+                    transport = new MotorcycleTransport();
+                    break;
+                case "--eats":
+                    transport = new BicycleTransport();
+                    break;
+                default:
+                    Console.WriteLine("Selecione o tipo da entrega.");
+                    break;
+            }
+
+            if (transport != null)
+            {
+                transport.StartTransport();
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ FactoryMethod/Vehicles/Bicycle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Bicycle : IVehicle
+    {
+        public void GetCargo()
+        {
+            Console.WriteLine("Pegamos os passageiros de bicicleta!");
+        }
+
+        public void StartRoute()
+        {
+            Console.WriteLine("Iniciando o trajeto.");
+            GetCargo();
+        }
+    }
+}
